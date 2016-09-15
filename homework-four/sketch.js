d662d3d1e2543a76e8c3f0ede7455b0b100f1936
Rori Kotch <footballlvr1@Roris-MBP.attlocal.net>
2016-09-14 22:15:35 -0400

    started homework 4
    made the colored boxes and established the boundaries and colored
    rectangles

--- homework-four/sketch.js
+++ homework-four/sketch.js
@@ -1,7 +1,30 @@
+var boundary0 = 0;
+var boundary1 = 0;
+var boundary2 = 0;
+var boundary3 = 0;
+var topBoundary = 0;
+var bottomBoundary = 0; 
+
+var buttonSize = 100; 
+
 function setup() {
-  
+  createCanvas (400,500);
+  boundary0 = 0;
+  boundary1 = 100;
+  boundary2 = 200;
+  boundary3 = 300;
+  topBoundary = 400;
+  bottomBoundary = height;
+  noStroke();
 }
 
 function draw() {
-  
+  fill("green");
+  rect(boundary0, topBoundary, buttonSize, buttonSize);
+  fill ("purple");
+  rect (boundary1, topBoundary, buttonSize, buttonSize);
+  fill ("orange");
+  rect (boundary2, topBoundary, buttonSize, buttonSize);
+  fill ("yellow");
+  rect (boundary3, topBoundary, buttonSize, buttonSize);
 }
