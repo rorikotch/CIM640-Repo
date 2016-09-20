a1e95c80ed54431ba628a03ce34fb8d455104d58
Rori Kotch <footballlvr1@Roris-MBP.attlocal.net>
2016-09-20 18:38:38 -0400

    updated old project and added new project

--- homework-four/sketch.js
+++ homework-four/sketch.js
@@ -3,17 +3,17 @@ var boundary1 = 0;
 var boundary2 = 0;
 var boundary3 = 0;
 var topBoundary = 0;
-var bottomBoundary = 0; 
+var bottomBoundary = 0;
 
 var brushColor = "black";
 
 var currentArea = "";
 
 
-var buttonSize = 100; 
+var buttonSize = 100;
 
 function setup() {
-  createCanvas (400,500);
+  createCanvas(400, 500);
   boundary0 = 0;
   boundary1 = 100;
   boundary2 = 200;
@@ -26,15 +26,45 @@ function setup() {
 function draw() {
   fill("green");
   rect(boundary0, topBoundary, buttonSize, buttonSize);
-  fill ("purple");
-  rect (boundary1, topBoundary, buttonSize, buttonSize);
-  fill ("orange");
-  rect (boundary2, topBoundary, buttonSize, buttonSize);
-  fill ("yellow");
-  rect (boundary3, topBoundary, buttonSize, buttonSize);
-  
+  fill("purple");
+  rect(boundary1, topBoundary, buttonSize, buttonSize);
+  fill("orange");
+  rect(boundary2, topBoundary, buttonSize, buttonSize);
+  fill("yellow");
+  rect(boundary3, topBoundary, buttonSize, buttonSize);
+
   fill(brushColor);
   ellipse(mouseX, mouseY, 10, 10);
-  console.log(currentArea);
-  
+  //console.log(currentArea);
+
+  if (mouseY >= topBoundary && mouseY < bottomBoundary) {
+    console.log("bottom");
+    if (mouseX > boundary0 && mouseX < boundary1) {
+      console.log("area0");
+      currentArea = "area0";
+    } else if (mouseX > boundary1 && mouseX < boundary2) {
+      console.log("area1");
+      currentArea = "area1";
+    } else if (mouseX > boundary1 && mouseX < boundary3) {
+      console.log("area2");
+      currentArea = "area2";
+    } else if (mouseX > boundary2 && mouseX <= width)
+      console.log("area3");
+    currentArea = "area3";
+  }
+
+
+
+  function mousePressed() {
+    console.log(currentArea);
+    if (currentArea == "area0") {
+      brushColor = "green";
+    } else if (currentArea == "area1") {
+      brushColor = "purple";
+    } else if (currentArea == "area2") {
+      brushColor = "orange";
+    } else if (currentArea == "area3") {
+      brushColor = "yellow";
+    }
+  }
 }
