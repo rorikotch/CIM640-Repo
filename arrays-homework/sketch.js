c19318184665ed6a90c057c21609cb2caa910943
Rori Kotch <footballlvr1@Roris-MBP.attlocal.net>
2016-09-22 19:42:29 -0400

    added arrays homework

---
+++ arrays-homework/sketch.js
@@ -0,0 +1,35 @@
+var randomPositionX = 0;
+var randomPositionY = 0;
+var numberOfRect = 300;
+var r, g, b;
+
+function setup() {
+  createCanvas(400, 400);
+  r = random(255);
+  b = random(255);
+  g = random(255);
+  noStroke();
+
+  background(255);
+  for (var i = 0; i < numberOfRect; i++) {
+    randomPositionX = random(width);
+    randomPostitionY = random(height);
+    rect(randomPositionX, randomPostitionY, 30, 20);
+    fill(r, g, b, 130)
+
+  }
+}
+
+function draw() {
+  fill(r, g, b, 127);
+}
+
+function mousePressed() {
+  // if (mouseIsPressed == true)
+  //console.log("thisworks")
+  if (mousePressed == true);
+
+    r = random(255);
+    b = random(255);
+    g = random(255);
+  }
