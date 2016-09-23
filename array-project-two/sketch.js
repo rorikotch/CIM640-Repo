3a5d14ed94df3eb26c656039787674c88452ec82
Rori Kotch <footballlvr1@Roris-MBP.attlocal.net>
2016-09-22 22:57:49 -0400

    changed brush color
    made the brush color black

--- array-project-two/sketch.js
+++ array-project-two/sketch.js
@@ -3,7 +3,7 @@ var randomPositionX = 0;
 var randomPositionY = 0;
 var numberOfRect = 300;
 var brushSize = 20; 
-var brushColor = ""; 
+var brushColor = "black"; 
 
 function setup() {
   createCanvas(720, 400);
@@ -23,8 +23,7 @@ function draw() {
   ellipse(randomPositionX, randomPositionY, 150, 150);
   ellipse(randomPositionX, randomPositionY, 100, 100);
   ellipse(randomPositionX, randomPositionY, 50, 50);
-
-  frameRate(4);
+  frameRate(3);
   
   fill(brushColor);
   ellipse(mouseX, mouseY, brushSize, brushSize);
@@ -37,6 +36,6 @@ function mousePressed() {
     g = random(255);
     b = random(255);
   } if (mousePressed === true);
-    brushColor = r, g, b;
+    brushColor = (0);
 
 }
