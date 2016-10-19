c31d03524892a169da916c5949114dee01d73b08
Rori Kotch <footballlvr1@Roris-MBP.attlocal.net>
2016-10-19 16:43:31 -0400

    kim helped

--- midterm /midterm/sketch.js
+++ midterm /midterm/sketch.js
@@ -1,5 +1,6 @@
 var prevMillis = 0;
-var interval = 5000;
+var interval = 100;
+var reset = 200;
 var counter = 0;
 
 function setup() {
@@ -7,7 +8,15 @@ function setup() {
 }
 
 function draw() {
-  fill (0);
+
+  counter++;
+  
+    //make a button to reset and the reset would be to set the previous millis equal to millis, so setting the time to start from right now. 
+
+  if (counter <= interval){
+    console.log("counter;="+counter);
+    
+      fill (0);
   rect (500,400,500,100);
   //face circle
   fill(0,55,200);
@@ -26,12 +35,36 @@ function draw() {
   fill(0);
   ellipse(955, 470, 30, 20);
   
-  //make a button to reset and the reset would be to set the previous millis equal to millis, so setting the time to start from right now. 
-  if (millis() - prevMillis >= interval){
-    counter++;
-    console.log(counter);
-    prevMillis = millis();
+  
+  } else {
+    
+    console.log("counter-white;="+counter);
+      fill (255);
+  rect (500,400,500,100);
+  //face circle
+  fill(255);
+  ellipse (950,450,80,80);
+  //left eye
+  fill(255);
+  ellipse(938,445, 20, 20);
+  //right eye
+  ellipse(958, 445, 20, 20); 
+  //left pupil
+  fill(255);
+  ellipse(938, 445, 10, 10);
+  //right pupil
+  ellipse(958, 445, 10, 10);
+  //mouth 
+  fill(255);
+  ellipse(955, 470, 30, 20);
+    
+    if (counter >= reset){
+      console.log("reset")
+      counter = 0;
+    }
+    
   } 
+
 }
 
 
