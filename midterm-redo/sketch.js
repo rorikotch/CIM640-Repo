5a4c957618ccd5d6c320f9dc82854113d9c9fd29
Rori Kotch <r.kotch1@umiami.edu>
2016-10-21 13:17:26 -0400

    it closed so i had to re-save

--- midterm-redo/sketch.js
+++ midterm-redo/sketch.js
@@ -2,7 +2,7 @@ var prevMillis = 0;
 var interval = 100;
 var reset = 200;
 var counter = 0;
-var words = ["Take a break","Eye know you can do it","Take your vision to the next level","Look away","See what everyone else is up to for a minute","Go get coffee"];
+var words = ["Take a break", "Eye know you can do it", "Take your vision to the next level"];
 var word = random(words); 
 
 
@@ -67,9 +67,10 @@ function draw() {
   } 
   
   fill(255);
-  text(random(words), 510, 450);
+  text(words, 510, 450);
   textStyle(BOLD);
   textSize(13);
+  strokeWeight(0);
 
 
 }
