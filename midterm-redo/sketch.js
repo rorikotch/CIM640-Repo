5aac5fd5450a55e6e150042d3e62fe68112d53c7
Rori Kotch <r.kotch1@umiami.edu>
2016-10-21 11:57:19 -0400

    styled the text

--- midterm-redo/sketch.js
+++ midterm-redo/sketch.js
@@ -2,7 +2,7 @@ var prevMillis = 0;
 var interval = 100;
 var reset = 200;
 var counter = 0;
-var words = ["take a break", "eye know you can do it", "take your vision to the next level"];
+var words = ["Take a break", "Eye know you can do it", "Take your vision to the next level"];
 var word = random(words); 
 
 
@@ -65,10 +65,12 @@ function draw() {
       counter = 0;
     }
   } 
-
+  
   fill(255);
   text(words, 510, 450);
-  
+  textStyle(BOLD);
+  textSize(13);
+
 
 }
 
