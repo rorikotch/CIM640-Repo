931fbb1a19ba5f6799ada5afc4526471518e925f
Rori Kotch <r.kotch1@umiami.edu>
2016-10-21 15:16:31 -0400

    added more sayings

--- midterm-redo/sketch.js
+++ midterm-redo/sketch.js
@@ -2,7 +2,7 @@ var prevMillis = 0;
 var interval = 100;
 var reset = 200;
 var counter = 0;
-var words = ["Take a break", "Eye know you can do it", "Take your vision to the next level"];
+var words = ["Take a break", "Eye know you can do it", "Take your vision to the next level","Go get a coffee", "See what your co-workers are doing for a minute"];
 var word = random(words); 
 
 
