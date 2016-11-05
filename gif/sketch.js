e39be0891b2df55362caff5d3679506695632801
Rori Kotch <r.kotch1@umiami.edu>
2016-11-05 16:02:17 -0400

    loaded gifs into file, but still getting error

--- gif/sketch.js
+++ gif/sketch.js
@@ -1,11 +1,30 @@
 var gif;
+mousePressed = true; 
 
 function setup() {
   createCanvas(600,600);
-  gif = loadGif('mygif-one.gif');
+  image(gif, 10, 10);
+}
+
+function preload() {
+  img = loadImage("mygif-one.gif");
+  img = loadImage("mygif-two.gif");
+  img = loadImage("mygif-three.gif");
 }
 
 function draw() {
-  background(0);
-  image(gif, 0, 0);
+  //background(0);
+  
+  if (mousePressed === true) {
+  console.log('working');
+  gif = loadImage('mygif-one.gif');
+  gif= loadImage('mygif-two.gif');
+  gif = loadImage('mygif-three.gif');
+  }
+
+  else {
+  (mousePressed === false);
+  background(255); 
+  }
 }
+
