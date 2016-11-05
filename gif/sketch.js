944c122fa4d595bd934e567af4f63a660624afe6
Rori Kotch <r.kotch1@umiami.edu>
2016-11-05 16:04:02 -0400

    moved preload before setup

--- gif/sketch.js
+++ gif/sketch.js
@@ -1,15 +1,15 @@
 var gif;
 mousePressed = true; 
 
-function setup() {
-  createCanvas(600,600);
-  image(gif, 10, 10);
-}
-
 function preload() {
   img = loadImage("mygif-one.gif");
   img = loadImage("mygif-two.gif");
   img = loadImage("mygif-three.gif");
+}
+
+function setup() {
+  createCanvas(600,600);
+  image(gif, 100, 100);
 }
 
 function draw() {
