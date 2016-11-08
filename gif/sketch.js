095b0af2bbd644d8f5536e4bcb1da2ace6a51191
Rori Kotch <r.kotch1@umiami.edu>
2016-11-08 12:40:40 -0500

    fixed conditionals

--- gif/sketch.js
+++ gif/sketch.js
@@ -2,12 +2,17 @@ var gif;
 var gif1;
 var gif2;
 var loadGif; 
-
+var imageArray = [];
+var showImage = true;
+var mousePressed;
 
 function preload() {
   gif = loadGif("images/image-one.gif");
   gif1 = loadGif("images/image-two.gif");
   gif2 = loadGif("images/image-three.gif");
+  imageArray.push(loadImage('images/image-one.gif'));
+  imageArray.push(loadImage('images/image-two.gif'));
+  imageArray.push(loadImage('images/image-three.gif'));
 }
 
 function setup() {
@@ -16,10 +21,17 @@ function setup() {
 }
 
 function draw() {
+ 
+   
+  if (mouseIsPressed == true && showImage == true) {
   console.log('working');
   image(gif, 0, 0);
   image(gif1, 0, 0);
   image (gif2, 0,0);
+  } else {
+    background(255);
+  }
+  
   }
 
 
