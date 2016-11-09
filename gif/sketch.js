64d15fde83ce7645269e14b2b6aaf6ddab4015b7
Rori Kotch <r.kotch1@umiami.edu>
2016-11-09 11:04:38 -0500

    added more gifs

--- gif/sketch.js
+++ gif/sketch.js
@@ -2,7 +2,12 @@ var gif;
 var gif1;
 var gif2;
 var gif3;
-var loadGif; 
+var gif4;
+var gif5;
+var gif6;
+var loadGif;
+var randomPositionX = 0;
+var randomPositionY = 0;
 var imageArray = [];
 var showImage = true;
 var mousePressed;
@@ -12,30 +17,42 @@ function preload() {
   gif1 = loadGif("images/image-two.gif");
   gif2 = loadGif("images/image-three.gif");
   gif3 = loadGif("images/image-four.gif");
+  gif4 = loadGif("images/image-five.gif");
+  gif5 = loadGif("images/image-six.gif");
+  gif6 = loadGif("images/image-seven.gif");
   imageArray.push(loadImage('images/image-one.gif'));
   imageArray.push(loadImage('images/image-two.gif'));
   imageArray.push(loadImage('images/image-three.gif'));
   imageArray.push(loadImage('images/image-four.gif'));
+  imageArray.push(loadImage('images/image-five.gif'));
+  imageArray.push(loadImage('images/image-six.gif'));
+  imageArray.push(loadImage('images/image-seven.gif'));
+
+
 }
 
 function setup() {
-  createCanvas(1000,1000);
- 
+  createCanvas(1000, 1000);
+
 }
 
 function draw() {
- 
-   
+
+
   if (mouseIsPressed == true && showImage == true) {
-  console.log('working');
-  image(gif, 0, 0);
-  image(gif1, 10, 200);
-  image (gif2, 300,0);
-  image(gif3, 600, 10);
+    console.log('working');
+    image(gif, 0, 0);
+    image(gif1, 10, 200);
+    image(gif2, 300, 0);
+    image(gif3, 600, 10);
+    image(gif4, 500, 100);
+    image(gif5, 200, 500);
+    image(gif6, 500, 100);
+
+
+
   } else {
     background(255);
   }
-  
-  }
 
-
+}
