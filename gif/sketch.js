46e691f02a177c492e5b87c5d874385944ed7b82
Rori Kotch <r.kotch1@umiami.edu>
2016-11-09 10:52:55 -0500

    added another gif

--- gif/sketch.js
+++ gif/sketch.js
@@ -1,6 +1,7 @@
 var gif;
 var gif1;
 var gif2;
+var gif3;
 var loadGif; 
 var imageArray = [];
 var showImage = true;
@@ -10,13 +11,15 @@ function preload() {
   gif = loadGif("images/image-one.gif");
   gif1 = loadGif("images/image-two.gif");
   gif2 = loadGif("images/image-three.gif");
+  gif3 = loadGif("images/image-four.gif");
   imageArray.push(loadImage('images/image-one.gif'));
   imageArray.push(loadImage('images/image-two.gif'));
   imageArray.push(loadImage('images/image-three.gif'));
+  imageArray.push(loadImage('images/image-four.gif'));
 }
 
 function setup() {
-  createCanvas(600,600);
+  createCanvas(1000,1000);
  
 }
 
@@ -28,6 +31,7 @@ function draw() {
   image(gif, 0, 0);
   image(gif1, 10, 200);
   image (gif2, 300,0);
+  image(gif3, 600, 10);
   } else {
     background(255);
   }
