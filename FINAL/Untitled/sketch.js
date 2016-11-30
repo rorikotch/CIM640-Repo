0dc6c12169a11bbe6bedda03c74cb6ab8a56419b
Rori Kotch <r.kotch1@umiami.edu>
2016-11-29 19:27:01 -0500

    added code

--- FINAL/Untitled/sketch.js
+++ FINAL/Untitled/sketch.js
@@ -1,10 +1,27 @@
+	var myRec = new p5.SpeechRec();
+
 function setup() {
-  createCanvas(400,400);
+  	// graphics stuff:
+		createCanvas(800, 400);
+		background(255, 255, 255);
+		fill(0, 0, 0, 255);
+		// instructions:
+		textSize(32);
+		textAlign(CENTER);
+		text("say something", width/2, height/2);
+		myRec.onResult = showResult;
+		myRec.start();
 }
 
 function draw() {
-  fill(0);
-  rect(10,10, 300, 75);
-  
-  
-}
+  /*fill(0);
+  rect(10,10, 300, 75);*/
+} 
+
+function showResult()	{
+		if(myRec.resultValue==true) {
+			background(192, 255, 192);
+			text(myRec.resultString, width/2, height/2);
+			console.log(myRec.resultString);
+		}
+	}
